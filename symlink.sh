2db07ebd7f37761111f91b140ef96328c7b8345f
Tom Barone <mail@tombarone.net>
2023-09-09 09:25:24 +0200

    Add ability to remove the symlinks

--- symlink.sh
+++ symlink.sh
@@ -16,14 +16,24 @@ dotfiles_to_symlink=(
 
 make_link() {
 	directory=$1
-	stow -v -R -t "$HOME" "${directory}"
+	stow --verbose --restow --target "$HOME" "${directory}"
 }
 
-echo "Symlinking dotfiles..."
+delete_link() {
+	directory=$1
+	stow --verbose --delete --target "$HOME" "${directory}"
+}
 
 for directory in "${dotfiles_to_symlink[@]}"; do
-	make_link "$directory" || {
-		echo 'Symlinking failed'
-		exit 1
-	}
+	if [[ $1 == "--delete" ]]; then
+		delete_link "$directory" || {
+			echo 'Removing symlinks failed'
+			exit 1
+		}
+	else
+		make_link "$directory" || {
+			echo 'Symlinking failed'
+			exit 1
+		}
+	fi
 done
