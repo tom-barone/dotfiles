ee5a21f32d88b3cc2822c14a58e6fbbe79dfd06c
Tom Barone <mail@tombarone.net>
2024-07-07 14:19:04 +0930

    Add prettier to conform

--- nvim/.config/nvim/plugin/conform.lua
+++ nvim/.config/nvim/plugin/conform.lua
@@ -1,6 +1,8 @@
 require("conform").setup({
 	formatters_by_ft = {
 		python = { "tidy_imports", "ruff_fix", "ruff_format", "ruff_organize_imports" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
 	},
 })
 
