6bf606689156c3afa4d178b529ea5ff9d03fd181
Tom Barone <mail@tombarone.net>
2025-07-18 18:25:44 +0930

    Set ctrl-space to open autocomplete

--- nvim/.config/nvim/plugin/blink.lua
+++ nvim/.config/nvim/plugin/blink.lua
@@ -2,19 +2,47 @@ require("blink.cmp").setup({
 	-- https://cmp.saghen.dev/configuration/keymap.html
 	keymap = {
 		preset = "none",
-		["<Tab>"] = { "insert_next", "fallback" },
-		["<S-Tab>"] = { "insert_prev", "fallback" },
+		["<Tab>"] = {
+			-- If completion is not visible, do a regular tab
+			function(cmp)
+				if cmp.is_menu_visible() then
+					cmp.insert_next()
+					return true
+				end
+			end,
+			"fallback",
+		},
+		["<S-Tab>"] = {
+			function(cmp)
+				if cmp.is_menu_visible() then
+					cmp.insert_prev()
+					return true
+				end
+			end,
+			"fallback",
+		},
 		["<CR>"] = { "select_and_accept", "fallback" },
 		["<C-n>"] = { "scroll_documentation_down", "snippet_forward", "fallback" },
 		["<C-p>"] = { "scroll_documentation_up", "snippet_backward", "fallback" },
+		-- On macOS, <C-space> is reserved for switching input sources, so that needs to
+		-- be disabled before using it here.
+		["<C-Space>"] = {
+			"show",
+			"hide",
+			"fallback",
+		},
 	},
 	-- https://cmp.saghen.dev/configuration/reference.html
 	completion = {
 		list = {
-			selection = { preselect = false, auto_insert = true },
+			selection = {
+				preselect = false,
+				auto_insert = true,
+			},
 		},
 		menu = {
 			border = "rounded",
+			auto_show = false,
 		},
 		documentation = {
 			auto_show = true,
